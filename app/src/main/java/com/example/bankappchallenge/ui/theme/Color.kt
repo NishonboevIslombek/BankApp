a975588dbcf201c7bcddf322a32e5dc4fb315be4
islombeknishonboev <nishonboevislombek@gmail.com>
2023-09-23 20:55:41 +0500

    feat: Home Page implemented

--- app/src/main/java/com/example/bankappchallenge/ui/theme/Color.kt
+++ app/src/main/java/com/example/bankappchallenge/ui/theme/Color.kt
@@ -2,10 +2,10 @@ package com.example.bankappchallenge.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val PrimaryBlue = Color(0xFF3862F8)
+val LightBlue = Color(0xFFF4F5FA)
+val DarkBlue = Color(0xFF060F27)
+val Gray = Color(0xFF91949F)
+val MediumGray = Color(0xFFB2B7C7)
+val LightGray = Color(0xFFEBEDF6)
---
+++ app/src/main/java/com/example/bankappchallenge/ui/theme/Shape.kt
@@ -0,0 +1,11 @@
+package com.example.bankappchallenge.ui.theme
+
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Shapes
+import androidx.compose.ui.unit.dp
+
+val shapes = Shapes(
+    small = RoundedCornerShape(12.dp),
+    medium = RoundedCornerShape(16.dp),
+    large = RoundedCornerShape(topStart = 32.dp, topEnd = 32.dp)
+)

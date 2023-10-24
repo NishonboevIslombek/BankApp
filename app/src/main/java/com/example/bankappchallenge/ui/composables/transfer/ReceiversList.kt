dd6500d5a2a50c5c0b6535b140593f5b33896b42
islombeknishonboev <nishonboevislombek@gmail.com>
2023-10-24 12:35:40 +0500

    feat: ReceiversElement clickable behavior changed

--- app/src/main/java/com/example/bankappchallenge/ui/composables/transfer/ReceiversList.kt
+++ app/src/main/java/com/example/bankappchallenge/ui/composables/transfer/ReceiversList.kt
@@ -2,6 +2,8 @@ package com.example.bankappchallenge.ui.composables.transfer
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
@@ -13,6 +15,7 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -22,24 +25,31 @@ import androidx.compose.ui.unit.dp
 import com.example.bankappchallenge.R
 
 @Composable
-fun ReceiversColumn() {
+fun ReceiversColumn(onReceiverClicked: (id: Int) -> Unit) {
     LazyColumn(
         verticalArrangement = Arrangement.spacedBy(24.dp),
         contentPadding = PaddingValues(vertical = 32.dp)
     ) {
         items(50) {
-            ReceiversElement()
+            ReceiversElement(
+                onReceiverClicked = { onReceiverClicked(it) }
+            )
         }
     }
 }
 
 @Composable
-fun ReceiversElement(modifier: Modifier = Modifier) {
+fun ReceiversElement(modifier: Modifier = Modifier, onReceiverClicked: () -> Unit) {
     Row(
         verticalAlignment = Alignment.CenterVertically,
         modifier = modifier
             .background(MaterialTheme.colorScheme.primary)
             .padding(horizontal = 16.dp)
+            .clickable(
+                indication = null,
+                interactionSource = remember { MutableInteractionSource() },
+                onClick = onReceiverClicked
+            )
     ) {
         Image(
             painter = painterResource(id = R.drawable.png_avatar),

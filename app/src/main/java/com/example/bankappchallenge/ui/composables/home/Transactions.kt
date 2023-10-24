adeed27887905fdad71ad8645dd98036c771a0a5
islombeknishonboev <nishonboevislombek@gmail.com>
2023-10-24 12:37:45 +0500

    feat: new BottomSheetScaffold added to Transactions.kt

--- app/src/main/java/com/example/bankappchallenge/ui/composables/home/Transactions.kt
+++ app/src/main/java/com/example/bankappchallenge/ui/composables/home/Transactions.kt
@@ -5,41 +5,60 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.BottomSheetScaffold
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.material3.rememberBottomSheetScaffoldState
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.draw.shadow
-import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import com.example.bankappchallenge.R
 import com.example.bankappchallenge.ui.theme.MediumGray
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun RecentTransactionsSection(modifier: Modifier = Modifier) {
-    Surface(
-        color = MaterialTheme.colorScheme.primary,
-        contentColor = Color.Black,
-        shape = MaterialTheme.shapes.large,
-        modifier = modifier
-            .fillMaxSize()
-            .shadow(8.dp, shape = MaterialTheme.shapes.extraLarge)
-    ) {
-        Column(
-            horizontalAlignment = Alignment.CenterHorizontally,
-            modifier = Modifier
-        ) {
+fun RecentTransactionsSection(modifier: Modifier = Modifier, sheetPeakHeight: Dp) {
+    BottomSheetScaffold(
+        modifier = Modifier.nestedScroll(TopAppBarDefaults.enterAlwaysScrollBehavior().nestedScrollConnection),
+        sheetContent = {
+            Column(
+                horizontalAlignment = Alignment.CenterHorizontally,
+                modifier = Modifier
+            ) {
+                Row(verticalAlignment = Alignment.CenterVertically) {
+                    Text(
+                        text = "Recent Transactions",
+                        style = MaterialTheme.typography.titleMedium,
+                        modifier = Modifier
+                            .weight(1f)
+                            .padding(start = 16.dp)
+                    )
+                    Icon(
+                        painter = painterResource(id = R.drawable.ic_search),
+                        contentDescription = "Search",
+                        modifier = Modifier
+                            .size(56.dp)
+                            .padding(18.dp)
+                    )
+                }
+                TransactionsColumn()
+            }
+        },
+        sheetDragHandle = {
             Icon(
                 painter = painterResource(id = R.drawable.ic_maximize_gray),
                 contentDescription = "Maximize",
@@ -48,44 +67,31 @@ fun RecentTransactionsSection(modifier: Modifier = Modifier) {
                     .padding(top = 16.dp)
                     .size(50.dp)
             )
+        },
+        scaffoldState = rememberBottomSheetScaffoldState(),
+        sheetPeekHeight = sheetPeakHeight
+    ) {
 
-            Row(verticalAlignment = Alignment.CenterVertically) {
-                Text(
-                    text = "Recent Transactions",
-                    style = MaterialTheme.typography.titleMedium,
-                    modifier = Modifier
-                        .weight(1f)
-                        .padding(start = 16.dp)
-                )
-                Icon(
-                    painter = painterResource(id = R.drawable.ic_search),
-                    contentDescription = "Search",
-                    modifier = Modifier
-                        .size(56.dp)
-                        .padding(18.dp)
-                )
-            }
-
-            TransactionsColumn()
-        }
     }
 }
 
 @Composable
 fun TransactionsColumn(modifier: Modifier = Modifier) {
+    val lazyColumnState = rememberLazyListState()
     LazyColumn(
         verticalArrangement = Arrangement.spacedBy(16.dp),
         contentPadding = PaddingValues(vertical = 6.dp),
+        state = lazyColumnState,
         modifier = modifier
     ) {
         items(50) {
-            TransactionElement()
+            TransactionElement(id = it)
         }
     }
 }
 
 @Composable
-fun TransactionElement(modifier: Modifier = Modifier) {
+fun TransactionElement(modifier: Modifier = Modifier, id: Int) {
     Row(
         verticalAlignment = Alignment.CenterVertically,
         modifier = modifier
@@ -105,7 +111,7 @@ fun TransactionElement(modifier: Modifier = Modifier) {
                 .weight(1f)
         ) {
             Text(
-                text = "Netflix",
+                text = "Netflix $id",
                 style = MaterialTheme.typography.titleSmall
             )
             Text(
